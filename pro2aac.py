8edc5b5f1acbc42bc3dccf5efa25ba8461c5c557
kumardeep27 <ckumardeep@gmail.com>
2017-10-04 15:21:51 -1000

    1. template drawn

---
+++ pro2aac.py
@@ -0,0 +1,28 @@
+
+class LoadData():
+	def __init__(self):
+		pass
+	def fasta2sfasta(self,filepath):
+		#fasta 2 sfasta conversion, 
+		pass
+
+	def sfasta2aac(self):
+		#input sfasta, return df with header, rownames aac.
+		pass
+	
+	def runSVM(self):
+		#ip: df with features, op: prob. score df
+		pass
+
+	def masterOUT(self):
+		#ip: prob. score df, op: OUT file
+		pass
+
+data = LoadData()
+
+data.load_data('../data/protein.fasta')
+
+
+
+
+
