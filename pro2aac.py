b0474fbe2c89aeb9ff4e5f6a6920818dcb97f7dd
kumardeep27 <ckumardeep@gmail.com>
2017-10-05 11:52:51 -1000

    second module about to finish

--- pro2aac.py
+++ pro2aac.py
@@ -2,48 +2,6 @@
 import sys
 from pro2aac_backend import LoadData
 
-#class LoadData():
-#	def __init__(self):
-#		pass
-#	def fasta2sfasta(self,filepath):
-#		dictt={}
-#		#fasta 2 sfasta conversion, 
-#		pass
-#		with open(filepath, 'r') as fin:
-#			fin = map(str.rstrip, fin)
-#			for line in fin:
-#				line.rstrip('')
-#				#print(line,end="")
-#				#line.strip('\n').strip('\n')
-#				if line.startswith('>'):
-#					#line=map(str.rstrip,line)
-#					header=line
-#					#print(header)
-#					#print(line, end='',flush=True)
-#					sline=''
-#					#dictt[line]=line
-#				else:
-#					sline += line
-#					#print('\n')
-#					#print(sline,end="")
-#					#print('\n')         
-#					dictt[header]=sline
-#		return dictt			
-#
-#		
-#
-#	def sfasta2aac(self):
-#		#input sfasta, return df with header, rownames aac.
-#		pass
-#	
-#	def runSVM(self):
-#		#ip: df with features, op: prob. score df
-#		pass
-#
-#	def masterOUT(self):
-#		#ip: prob. score df, op: OUT file
-#		pass
-#	
 data = LoadData()
 
 #sfasta=data.fasta2sfasta('../data/proteins.fasta')
